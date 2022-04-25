d728f1cd96990d1690275d872822fa46eb104ba7
Evgeny <evgeny.rfi@gmail.com>
2022-04-26 00:11:22 +0200

    added *.sh files to rename project, need also to rename any occurency in Dockercompose file

--- renameProject.sh
+++ renameProject.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 #this script helps you to rename the template project to your custom name
 
-
-
-for f in $(find . -type f  \( -iname \*.conf -o -iname \*.py -o -iname \*.config -o -iname \*.conf -o -iname \*.yml -o -iname \*.compose \)  -not -path '*/venv/*')
+for f in $(find . -type f  \( -iname \*.conf -o -iname \*.py -o -iname \*.config -o -iname \*.conf -o -iname \*.yml -o -iname \*.compose -o -iname \*.sh \)  -not -path '*/venv/*')
 do
 sed -i.bak 's@mysite@NEWNAME@g' ${f}
 done
